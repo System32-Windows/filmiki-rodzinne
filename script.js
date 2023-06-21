5ec1eaafd012d393b1fa33298b4cc91fc957263b
System32-Windows <pango.land@outlook.com>
2023-06-21 17:55:27 +0200

    *

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function login(form) {
     }
     if ((form.username.value == "Jolanta") && (form.password.value == "Janiak")) {
         self.location.href = "success.html"
+    }
+    if ((form.username.value == "Katarzyna") && (form.password.value == "Długosz")) {
+        self.location.href = "success.html"
+    }
+    if ((form.username.value == "Daniel") && (form.password.value == "Długosz")) {
+        self.location.href = "success.html"
     } else {;
         document.getElementById("zakaz").style.display = "block"
         return false;
